04d495a2f06b349f4031856dc9aea78033b43d04
Sayantan <this.sayantan@gmail.com>
2023-11-23 16:11:30 +0700

    🔨 refactor(functions.py): remove unused imports and improve assistant creation function 📝 docs(functions.py): add detailed instructions for assistant creation to improve clarity and understanding

--- gpt_api/functions.py
+++ gpt_api/functions.py
@@ -1,45 +1,41 @@
-import os
 import json
-from openai import OpenAI
-
-# Init OpenAI Client
-client = OpenAI(api_key=OPENAI_API_KEY)
+import os
 
 
-# Create or load assistant
 def create_assistant(client):
-  assistant_file_path = 'assistant.json'
+    assistant_file_path = "assistant.json"
 
-  # If there is an assistant.json file already, then load that assistant
-  if os.path.exists(assistant_file_path):
-    with open(assistant_file_path, 'r') as file:
-      assistant_data = json.load(file)
-      assistant_id = assistant_data['assistant_id']
-      print("Loaded existing assistant ID.")
-  else:
-    # If no assistant.json is present, create a new assistant using the below specifications
+    if os.path.exists(assistant_file_path):
+        with open(assistant_file_path, "r") as file:
+            assistant_data = json.load(file)
+            assistant_id = assistant_data["assistant_id"]
+            print("Loaded existing assistant ID.")
+    else:
+        file = client.files.create(
+            file=open("./knowledgebase/data.md", "rb"), purpose="assistants"
+        )
 
-    # To change the knowledge document, modifiy the file name below to match your document
-    # If you want to add multiple files, paste this function into ChatGPT and ask for it to add support for multiple files
-    file = client.files.create(file=open("../knowledgebase/data.md", "rb"),
-                               purpose='assistants')
+        assistant = client.beta.assistants.create(
+            instructions="""
+          Role and Goal: GPT-4, designated as "Ava," functions as an advanced sales chatbot for bchurunway.com. Ava's interaction begins with a courteous and friendly greeting. To personalize the service, Ava will request the customer's name and a method of contact, such as an email or phone number. Ava is bilingual, capable of assisting customers in both English and Thai. Ava’s service is tailored to provide a bespoke and upscale shopping experience, with a focus on engaging high-society women as the primary clientele.
 
-    assistant = client.beta.assistants.create(
-        # Getting assistant prompt from "prompts.py" file, edit on left panel if you want to change the prompt
-        instructions=assistant_instructions,
-        model="gpt-4-1106-preview",
-        tools=[
-            {
-                "type": "retrieval"  # This adds the knowledge base as a tool
-            }
-        ],
-        file_ids=[file.id])
+          Constraints: Ava is programmed to refrain from disseminating false or outdated product data. This includes the prohibition of generating fictitious product image URLs. Ava's responses and information provision must be sourced from the pre-loaded knowledge base files to ensure accuracy, particularly with regards to product imagery and pricing, which is listed in Thai Baht. Ava must consistently exhibit a high level of courtesy and professionalism, mirroring the brand’s upscale ethos, and must not partake in off-topic conversations.
+          
+          Guidelines: Ava will leverage the knowledge base files to deliver precise details about products, services, visuals, prices, and policies. Ava will engage customers with a warm, amiable, yet informative demeanor, befitting an elite customer base.
+          
+          Clarification: When faced with ambiguity, Ava will seek clarification to provide exact and useful assistance. Ava is equipped to make informed assumptions when necessary, depending on context and the information at hand.
+          
+          Personalization: Ava will communicate in a sophisticated yet accessible tone. To enrich the customer experience, Ava will use customers' names and, when possible, draw on previous interactions.
+          """,
+            model="gpt-4-1106-preview",
+            tools=[{"type": "retrieval"}],
+            file_ids=[file.id],
+        )
 
-    # Create a new assistant.json file to load on future runs
-    with open(assistant_file_path, 'w') as file:
-      json.dump({'assistant_id': assistant.id}, file)
-      print("Created a new assistant and saved the ID.")
+        with open(assistant_file_path, "w") as file:
+            json.dump({"assistant_id": assistant.id}, file)
+            print("Created a new assistant and saved the ID.")
 
-    assistant_id = assistant.id
+        assistant_id = assistant.id
 
-  return assistant_id
+    return assistant_id
